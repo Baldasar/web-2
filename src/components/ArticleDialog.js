7defe3e878d2a027ae05aafd6cf7e7026b6261e4
osLeo <leonardo.spilere@deps.com.br>
2023-11-28 19:27:49 -0300

    alteração dialog e tela login

--- src/components/ArticleDialog.js
+++ src/components/ArticleDialog.js
@@ -1,18 +1,21 @@
 import React from "react";
 
 export function ArticleDialog({ article, onClose }) {
+
+  const handleDialogClick = (event) => {
+    event.stopPropagation();
+  };
+
   return (
-    <div className="article-dialog-overlay">
-      <div className="article-dialog">
-        <h2>{article.kb_title}</h2>
-        <br />
-        <p>{article.kb_body}</p>
-        <br />
-        <div id="interact">
+    <div className="article-dialog-overlay" onClick={onClose}>
+      <div className="article-dialog" onClick={handleDialogClick}>
+        <div id="dialog-header">
+          <h2>{article.kb_title}</h2>
           <button id="close-button" onClick={onClose}>
-            Fechar
+            X
           </button>
         </div>
+        <p>{article.kb_body}</p>
       </div>
     </div>
   );
